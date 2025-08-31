c051929b68cfc516477aad9c540cba92cd5ae5bf
Zzixuan711 <agneslee8491@gmail.com>
2025-08-31 17:43:01 +0800

    Update Patient.java

--- clinic management system/src/entity/Patient.java
+++ clinic management system/src/entity/Patient.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Implement a Patient class here
 package entity;
 
-/**
- *
- * @author Zixuan
- */
 public class Patient {
     
 }
+
